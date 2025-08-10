884c4f0164b4c1c8d7f718f53c0a6f86d1b39458
Rocco <popescu.rocco@yahoo.com>
2025-08-10 14:00:52 +0300

    story 3.1 - user input

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -1,7 +1,6 @@
 public class FirstCode {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!!");
+        System.out.println("This is a change!!");
+        System.out.println("Another change");
     }
 }
