3bbd516af30f399afcfdd513c8a970d07b8a6a16
Rocco <popescu.rocco@yahoo.com>
2025-08-10 14:12:32 +0300

    readme and story 3.2

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -2,5 +2,7 @@ public class FirstCode {
     public static void main(String[] args) {
         System.out.println("This is a change!!");
         System.out.println("Another change");
+
+        //please submit data to the database
     }
 }
